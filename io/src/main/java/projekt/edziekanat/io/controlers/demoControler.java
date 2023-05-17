3f9f9857777534793d6b26e99d264d7b12396194
1Batrex1 <bartosz.gajda@protonmail.com>
2023-05-17 19:35:00 +0200

    podstawowe pliki java

---
+++ io/src/main/java/projekt/edziekanat/io/controlers/demoControler.java
@@ -0,0 +1,14 @@
+package projekt.edziekanat.io.controlers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class demoControler {
+
+    @GetMapping("/")
+    String HelloWord()
+    {
+        return "Hello";
+    }
+}
---
+++ io/src/main/java/projekt/edziekanat/io/entites/Rola.java
@@ -0,0 +1,16 @@
+package projekt.edziekanat.io.entites;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+@Entity
+@Table(name = "Rola")
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor
+
+public class Rola {
+}
